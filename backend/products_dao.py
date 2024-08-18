31f48152bdc6549dca902e3f1967d1b8ff9bde47
M0hammed-Gamal <m_m20000075@yahoo.com>
2024-08-18 17:27:53 +0300

    test mysql connection to fitch all data

---
+++ backend/products_dao.py
@@ -0,0 +1,22 @@
+from sql_connection import get_sql_connection
+
+def get_all_products(connection):
+    cursor = connection.cursor()
+    query = ("select product.product_id, product.name, product.uom_id, product.price_per_unit, uom.uom_name from product inner join uom on product.uom_id=uom.uom_id")
+    cursor.execute(query)
+    response = []
+    for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
+        response.append({
+            'product_id': product_id,
+            'name': name,
+            'uom_id': uom_id,
+            'price_per_unit': price_per_unit,
+            'uom_name': uom_name
+        })
+    return response
+
+
+if __name__ == '__main__':
+    connection = get_sql_connection()
+    print(get_all_products(connection))
+  
