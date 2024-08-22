22817d10bcf83a9aad25d4971de37ecacc758383
M0hammed-Gamal <m_m20000075@yahoo.com>
2024-08-22 15:09:30 +0300

    hot fix to change quick writting :)

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -1,8 +1,8 @@
 from sql_connection import get_sql_connection
 
-def get_all_products(connection):
+def get_all_product(connection):
     cursor = connection.cursor()
-    query = ("select products.product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name from products inner join uom on products.uom_id=uom.uom_id")
+    query = ("select product.product_id, product.name, product.uom_id, product.price_per_unit, uom.uom_name from product inner join uom on product.uom_id=uom.uom_id")
     cursor.execute(query)
     response = []
     for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
@@ -17,7 +17,7 @@ def get_all_products(connection):
 
 def insert_new_product(connection, product):
     cursor = connection.cursor()
-    query = ("INSERT INTO products "
+    query = ("INSERT INTO product "
              "(name, uom_id, price_per_unit)"
              "VALUES (%s, %s, %s)")
     data = (product['product_name'], product['uom_id'], product['price_per_unit'])
@@ -29,7 +29,7 @@ def insert_new_product(connection, product):
 
 def delete_product(connection, product_id):
     cursor = connection.cursor()
-    query = ("DELETE FROM products where product_id=" + str(product_id))
+    query = ("DELETE FROM product where product_id=" + str(product_id))
     cursor.execute(query)
     connection.commit()
 
@@ -37,9 +37,9 @@ def delete_product(connection, product_id):
 
 if __name__ == '__main__':
     connection = get_sql_connection()
-    # print(get_all_products(connection))
+    # print(get_all_product(connection))
     print(insert_new_product(connection, {
         'product_name': 'potatoes',
         'uom_id': '1',
-        'price_per_unit': 10
+        'price_per_unit': 13
     }))
